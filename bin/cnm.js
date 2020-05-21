00923b71cab8c6a0dfea0d5c67361db99f151fcd
leslie zhu <zhujunshuai@huohua.cn>
2020-05-21 10:44:00 +0800

    fix env call

--- bin/cnm.js
+++ bin/cnm.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 
-require('@babel/register')({
-    only: [/src/],
-    cache: false
-});
+var args = process.argv.slice(2);
 
-module.exports = require('../src/index.js');
+var env = /^--env=([a-zA-Z]+)$/g.exec(args);
+
+process.env.NODE_ENV = env && env[1];
+
+if (process.env.NODE_ENV === 'development') {
+    require('@babel/register')({
+        only: [/src/],
+        cache: false
+    });
+
+    module.exports = require('../src/index.js');
+} else {
+    require('@babel/register')({
+        only: [/dist/],
+        cache: false
+    });
+
+    module.exports = require('../dist/bundle.min');
+}
